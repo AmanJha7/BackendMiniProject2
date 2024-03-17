7aa4d7cca803bae9ffd9a38ce2d9bad1869630ed
Amanjha8503 <99260113+AmanJha7@users.noreply.github.com>
2024-03-18 00:12:57 +0530

    Updation and optimization of import stmts

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logmiddleware = require('./middleware/logindatastorer');
-const userRoutes = require('./routes/userRoutes');
-const blacklistRoutes = require('./routes/blacklistRoutes');
-const notificationRoutes = require('./routes/notificationRoutes');
-const postRoutes = require('./routes/postRoutes');
-const restrictToLoggedinUserOnly = require('./middleware/auth')
-const authRoutes = require('./routes/authRoutes')
+const  { express,bodyParser,restrictToLoggedinUserOnly,authRoutes,postRoutes,notificationRoutes,blacklistRoutes,userRoutes,logmiddleware,cookieParser,path,mongoose,} = require('./importer');
 require('dotenv').config()
 
 const app = express();
