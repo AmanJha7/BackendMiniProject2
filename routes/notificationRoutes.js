41e708a3349fc215baa5d79811eba0748a61e7b3
Amanjha8503 <99260113+AmanJha7@users.noreply.github.com>
2024-03-18 00:26:11 +0530

    feat: Add controllers for post operations
    Implemented controller functions to handle post creation, retrieval, updating, and deletion in the backend. Each operation is encapsulated within separate controller functions to maintain clean and modular code. This commit enhances code organization and readability, making it easier to manage post-related functionality within the application.

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,30 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Notification = require('../models/notification');
+const notificationController = require('../controller/notificationController');
 
 // Retrieve notifications for a user
-router.get('/:userId', async (req, res) => {
-    try {
-        const notifications = await Notification.find({ from: req.params.userId });
-        res.json(notifications);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-});
+router.get('/:userId', notificationController.getUserNotifications);
 
 // Mark notification as read
-router.put('/:notificationId', async (req, res) => {
-    try {
-        const notification = await Notification.findByIdAndUpdate(req.params.notificationId, { status: 'read' }, { new: true });
-        if (!notification) {
-            return res.status(404).json({ msg: 'Notification not found' });
-        }
-        res.json(notification);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-});
+router.put('/:notificationId', notificationController.markNotificationAsRead);
 
 module.exports = router;
